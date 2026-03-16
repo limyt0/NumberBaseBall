cf5a5a21cdcd3a0dfbafe26f1b874c3e83f893d2
limyt0 <dudx45@gmail.com>
2026-03-16 11:32:01 +0900

    core update

---
+++ Source/NumberBaseBall/Game/MyGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBase.generated.h"
+
+UCLASS()
+class NUMBERBASEBALL_API AMyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
